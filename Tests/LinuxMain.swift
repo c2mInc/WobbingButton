5cbeb47eaa90806bab99b6c896d0a552f15af712
Erk Ekin <erkekin@gmail.com>
2018-09-23 10:54:40 +0300

    first commit

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import WobbingButtonTests
+
+var tests = [XCTestCaseEntry]()
+tests += WobbingButtonTests.allTests()
+XCTMain(tests)
---
+++ WobbingButtonExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WobbingButtonExample
+//
+//  Created by Erk Ekin on 23.09.2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
